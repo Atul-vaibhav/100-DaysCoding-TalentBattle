03f243792705fb23cb5a579df52a8cfe429bfee4
Atul-vaibhav <atulvaibhav1234@gmail.com>
2024-10-20 18:05:30 +0530

    "Day 11 Code... "

---
+++ Day11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day11 {
+    public static int fibonacci(int n){
+        if(n==0||n==1){
+            return n;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+            System.out.print(fibonacci(i) +" ");
+        } 
+    }
+}
