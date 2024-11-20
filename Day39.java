e9b655730c87204bd1374043511cb447ff09fa4f
Atul-vaibhav <atulvaibhav1234@gmail.com>
2024-11-20 18:45:19 +0530

    " "

--- Day39.java
+++ Day39.java
@@ -1,3 +1,17 @@
+/* Day 39 coding Statement : Write a Program to check if two strings are Anagram or not
+
+Description
+
+Get two strings as input from the user and check whether it is Anagram or not.
+
+Input
+
+sunlight thgiluns
+
+Output
+
+Anagram  */
+
 import java.util.Scanner;
 
 public class Day39 {
@@ -7,12 +21,30 @@ public class Day39 {
         String str = sc.nextLine();
         System.out.println("Enter 2nd String: ");
         String str1 = sc.nextLine();
-        
-        for(int i=0; i<str.length(); i++){
-            char c = str.charAt(i);
+
+        if(isAnagram(str, str1)){
+            System.out.println("The Strings are Anagram ");
+        }else{
+            System.out.println("The Strings are not Anagram");
         }
-        for(int i=0; i<str1.length();i++){
-            char c1= str1.charAt(i);
+    }
+    public static boolean isAnagram(String str, String str1){
+        str = str.toLowerCase();
+        str1 = str1.toLowerCase();
+
+        if(str1.length() != str1.length()){
+            return false;
         }
+         int[] count = new int[256];
+         for(int i=0; i<str.length();i++){
+            count[str.charAt(i)]++;
+            count[str1.charAt(i)]--;
+         }
+        for(int i=0; i<256;i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
