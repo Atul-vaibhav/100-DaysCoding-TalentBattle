1edec0b9f7e3ae9f07d8f7b52a22ccd6998087d2
Atul-vaibhav <atulvaibhav1234@gmail.com>
2024-12-01 12:00:02 +0530

    "Day1 - Day100 Code"

--- Day45.java
+++ Day45.java
@@ -1,3 +1,29 @@
+/* Day 45 coding Statement : Write Program to find smallest and largest element in an array
+
+Description
+
+Get an array as input from the user and then find the smallest and largest element in the array.
+
+Input
+
+Enter the size of array :
+
+5
+
+Enter the elements :
+
+10 20 5 40 30
+
+Output
+
+Smallest Number :
+
+5
+
+Largest Number :
+
+40  */
+
 import java.util.Scanner;
 
 public class Day45 {
--- Day46.java
+++ Day46.java
@@ -1,3 +1,23 @@
+/* Day 46 coding Statement : Write Program to find sum of elements in an array
+
+Description
+
+Get an array as the input from the user and find the sum of the elements.
+
+Input
+
+Enter the size of array
+
+3
+
+Enter the elements of array
+
+5 10 15
+
+Output
+
+30  */
+
 import java.util.Scanner;
 
 public class Day46 {
